42d4145f4264ad3e9e9e36bc3d0af5960c67a991
umbur <work1415@gmail.com>
2019-02-04 11:20:00 -0700

    Created som basic file/components structure

---
+++ Front-end/coordinator-storytelling/src/components/MainContainer/MainPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class MainPage extends Component {
+
+  render() {
+    return (
+     <h1> This is the Main page</h1>
+    );
+  }
+
+}
+
+export default MainPage;
