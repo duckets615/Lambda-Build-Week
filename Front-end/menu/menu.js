d3cd7e62864058a6cda017bf5402363aed91b722
unknown <eriznis@gmail.com>
2019-02-05 18:10:32 -0500

    some minor fixes, will work on sign up page

--- Front-end/menu/menu.js
+++ Front-end/menu/menu.js
@@ -1,12 +1,19 @@
 const toggleMenu = () => {
-    menu.classList.toggle('menu--open')
-    // Toggle the "menu--open" class on your menu refence. 
-   }
+    menu.classList.toggle('menu--open');
+}
    
-   // Start Here: Create a reference to the ".menu" class
-   const menu = document.querySelector('.menu')
-   // create a reference to the ".menu-button" class
-   const menuButton = document.querySelector('.menu-button')
-   // Using your menuButton reference, add a click handler that calls toggleMenu
-   menuButton.addEventListener('click', toggleMenu)
+
+   
+   const menu = document.querySelector('.menu');
+   const menuButton = document.querySelector('.menu-button');
+   menuButton.addEventListener('click', toggleMenu);
+   
+
+
+   
+
+
+  
+
+   
    
