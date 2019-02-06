ab0475f0f7c20998d64465c827bb7ee76e63fe82
unknown <eriznis@gmail.com>
2019-02-06 14:40:22 -0500

    Changed JavaScript code

--- Front-end/JS/menu.js
+++ Front-end/JS/menu.js
@@ -1,19 +1,15 @@
-const toggleMenu = () => {
-    menu.classList.toggle('menu--open');
+class Menu {
+    constructor(element){
+        this.element = element;
+        this.menu = this.element.querySelector('.menu');
+        this.menuButton = this.element.querySelector('.menu-button');
+        this.menuButton.addEventListener('click', () => this.toggleMenu());
+        
+    }
+    toggleMenu () {
+        this.menu.classList.toggle('menu-open');
+    }
+    
 }
-   
 
-   
-   const menu = document.querySelector('.menu');
-   const menuButton = document.querySelector('.menu-button');
-   menuButton.addEventListener('click', toggleMenu);
-   
-
-
-   
-
-
-  
-
-   
-   
+let menus = document.querySelectorAll('.navigation').forEach(element => new Menu(element))
