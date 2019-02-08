eccb8b35b92ef386fa26d8cfeb6feb889340efea
umbur <work1415@gmail.com>
2019-02-08 16:28:56 -0700

    Matched the provided styling

--- Front-end/coordinator-storytelling/src/components/MainContainer/MainPage.js
+++ Front-end/coordinator-storytelling/src/components/MainContainer/MainPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
 
+import '../../CSS/home.css'
+
 class MainPage extends Component {
 
   render() {
     return (
-    <div>
+    <div className='main'>
       <Navigation />
       
     </div>
--- Front-end/coordinator-storytelling/src/components/MainContainer/Navigation.js
+++ Front-end/coordinator-storytelling/src/components/MainContainer/Navigation.js
@@ -3,53 +3,44 @@ import { Route, NavLink } from 'react-router-dom';
 
 import Content from './Content'
 import About from './About'
-import './Home.css'
+import '../../CSS/home.css'
 
 
 class Navigation extends React.Component{
-    handlePick(){
+    openHandler(){
         document.getElementById("sidenav").style.width = "250px";
-    
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
-    closePick(){
+    closeHandler(){
         document.getElementById("sidenav").style.width = "0";
         console.log('clicked!')
     }
     render(){
- return(
-     
-
-
-
-<div>
-    <nav>
-        <span className="menu" onClick={this.handlePick}>MENU</span>
-    <div id="sidenav"  className='sidenav'>
-    <span className="closebtn" id='sidenav' onClick={this.closePick}>X</span>
-        <NavLink exact to="/">
-              Home
-        </NavLink>
-        <NavLink to="/about">
-              About
-        </NavLink>  
+        return(
+            <div>
+              <header>
+                <nav>
+                    <span className="menu" onClick={this.openHandler}>MENU</span>
+                    <div id="sidenav"  className='sidenav'>
+                        <span className="closebtn" id='sidenav' onClick={this.closeHandler}>X</span>
+                        <NavLink exact to="/">
+                            Home
+                        </NavLink>
+                        <NavLink to="/about">
+                            About
+                        </NavLink>  
        
-        <a href="submitstory.html">Submit Story</a>
-        <a href="contact.html">Contact Us</a>
-        </div>
-    </nav>
-    <a className="signup" href="signup.html">Sign Up/Log In</a>
-    <Route exact path="/" component={Content} />
-    <Route exact path="/about" component={About} />
-</div>
-   
-
-
-
-
-
- )
-    }
+                        <a href="submitstory.html">Submit Story</a>
+                        <a href="contact.html">Contact Us</a>
+                    </div>
+                </nav>
+                <a className="sign" href="signup.html">Sign Up/Log In</a>
+              </header>
+               
+                <Route exact path="/" component={Content} />
+                <Route exact path="/about" component={About} />
+            </div>
+            )
+        }
 }
 
 export default Navigation;
