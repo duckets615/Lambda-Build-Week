4a4a873bf10893512db147d37322d3c594905edc
umbur <work1415@gmail.com>
2019-02-11 10:31:56 -0700

    Added about page

--- Front-end/coordinator-storytelling/src/components/MainContainer/About.js
+++ Front-end/coordinator-storytelling/src/components/MainContainer/About.js
@@ -4,9 +4,42 @@ import React from 'react';
 const About = () => {
     return(
      <div className='about'>
-  
-           test
-      
+        <div class="title">
+            <img src="/img/about-us-achievement-agreement-533405.jpg" alt='' />
+            <h1>More About Us</h1>
+    </div>
+    <div class="para">
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget urna malesuada risus bibendum tempor
+            ullamcorper eu velit. Fusce luctus mi in dictum tincidunt. Nam sollicitudin libero nec dui ultrices, non
+            pulvinar purus interdum. Phasellus venenatis mauris nec eleifend fermentum. Nunc ultricies urna at turpis
+            volutpat placerat. Nulla venenatis libero nibh, venenatis tincidunt lorem interdum sit amet. Nulla
+            facilisi. Etiam libero ligula, efficitur at nisi sed, porttitor vehicula enim. In in rhoncus metus, ut
+            fringilla tortor.</p>
+
+            <p> Aliquam tempus mi arcu, id convallis turpis vestibulum in. Sed maximus tristique ante hendrerit hendrerit.
+            Pellentesque fringilla eros non tincidunt tempor. Duis fringilla vehicula nunc, ut vestibulum neque
+            lobortis quis. Vivamus ac felis tristique augue volutpat convallis commodo nec odio. Aliquam ullamcorper at
+            lorem eget imperdiet. Ut suscipit quam vel velit malesuada viverra. Cras ac massa quis massa imperdiet
+            suscipit. Praesent nec nisl tincidunt, pellentesque urna non, pulvinar dolor. Suspendisse sit amet velit
+            aliquet, auctor risus ac, ultricies dui. Cras nisl erat, tempus in lectus at, viverra varius risus. Morbi
+            eget quam vulputate, euismod elit eget, ullamcorper sem. Duis hendrerit eleifend nibh, ac eleifend nibh
+            varius eget. Duis semper ex ligula, quis blandit justo elementum porttitor.</p>
+
+            <p> Nam a fermentum odio, nec laoreet sem. Nulla lacus tellus, tempus vitae ex sit amet, tincidunt lacinia
+            felis. Pellentesque ut nulla interdum, ultrices felis at, lobortis lacus. Mauris sit amet lacus lacus. Nunc
+            purus ante, volutpat nec molestie a, pretium et arcu. Sed rhoncus, nibh ut blandit dignissim, libero tortor
+            tincidunt justo, vel venenatis diam magna ut lorem. Nunc cursus semper elit, sit amet vulputate eros
+            rhoncus et. Vivamus vehicula pellentesque scelerisque. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos. Cras non ante at nibh porta mollis at dapibus libero. In varius,
+            velit sit amet tincidunt vestibulum, neque ipsum condimentum odio, nec ornare quam nunc quis diam.</p>
+    </div>
+    <div class="bottom">
+            <h3>Subscribe to our newsletter!</h3>
+        <div class="email">
+            <textarea placeholder='Email'></textarea>
+            <button>Submit</button>
+        </div>
+        </div>
 
 
      </div>
