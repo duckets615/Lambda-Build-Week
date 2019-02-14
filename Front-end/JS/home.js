0f027ade9f2fd3b444da99009245ccc4f8c16726
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-02-14 11:58:54 -0500

    added objects and an array.

--- Front-end/JS/home.js
+++ Front-end/JS/home.js
@@ -10,12 +10,57 @@ function openNav() {
     document.body.style.backgroundColor = "white";
   };
 
-  // const menu = document.querySelectorAll('.menu');
-  // menu.addEventListener('mouseover', e => {
-  //   e.target.style.color = "black";
-  // });
 
-  // menu.addEventListener('mouseleave', function (e) {
-  //   e.target.style.color = "rgba(224, 39, 39, 0.767)";
-  // });
+  const cards = [{"class":"firstCard", "person1":"Tigran", "person2":"Andrew", "person3":"Joe"},
+  {"class":"secondCard", "person":"Elan", "picture":"man on map"},
+  {"class":"thirdCard", "person":"Desi", "picture":"girl in grass field"},
+  {"class":"forthCard", "person":"Chris", "picture":"shaking hands"},
+  {"class":"topImg", "picture":"lake and mountains", "position":"fits page width"}]
 
+  console.log(cards);
+  // // concat()
+  // // var new_array = old_array.concat([value1[, value2[, ...[, valueN]]]])
+  // let item = cards.map(card => card.class)
+
+
+  const firstCard = {
+    person1:"Tigran",
+    person2:"Andrew",
+    person3:"Joe"
+  };
+
+  const secondCard = {
+    person: "Elan",
+    picture: "man on map"
+  };
+
+  const thirdCard = {
+    person: "Desi",
+    picture: "girl in grass field"
+  };
+
+  const forthCard = {
+    person: "Chris",
+    picture: "shaking hands"
+  };
+
+  const topImg = {
+    picture: "lake and mountains",
+    position: "fits page width"
+  };
+
+  const card = [firstCard, secondCard, thirdCard, forthCard, topImg];
+console.log(card);
+
+
+function Characteristics(attributes) {
+  this.name = attributes.name;
+  this.lastName = attributes.lastName;
+}
+
+const queen = new Characteristics({
+  name: "Destiny",
+  lastName: "Tibbitts",
+});
+
+console.log(queen.name)
--- Front-end/JS/object.js
+++ Front-end/JS/object.js
@@ -1,14 +1,38 @@
 
-  function Characteristics(attributes) {
-    this.name = attributes.name;
-    this.lastName = attributes.lastName;
-  }
-  
-  
-  
-  const queen = new Characteristics({
-    name: "Destiny",
-    lastName: "Tibbitts",
-  });
+// const firstCard = {
+//   person1: "Tigran",
+//   person2: "Andrew",
+//   person3: "Joe"
+// };
+// console.log(firstCard);
 
-  console.log(queen.name)
+// // try using .map()
+// const secondCard = {
+//   person: "Elan",
+//   picture: "man on map"
+// };
+// const thirdCard = {
+//   person: "Desi",
+//   picture: "girl in grass field"
+// };
+// const forthCard = {
+//   person: "Chris",
+//   picture: "shaking hands"
+// };
+// const topImg = {
+//   picture: "lake and mountains",
+//   position: "fits page"
+// };
+
+
+  // function Characteristics(attributes) {
+  //   this.name = attributes.name;
+  //   this.lastName = attributes.lastName;
+  // }
+
+  // const queen = new Characteristics({
+  //   name: "Destiny",
+  //   lastName: "Tibbitts",
+  // });
+
+  // console.log(queen.name)
